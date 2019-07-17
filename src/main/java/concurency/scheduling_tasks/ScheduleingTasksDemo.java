0fc039c6ed644d04d87c0cf6a026fc2e93fc9e04
a696986 <grigort@energizeglobal.com>
2019-07-17 20:02:00 +0400

    Added package concurrency.schedulling_tasks

---
+++ src/main/java/concurency/scheduling_tasks/ScheduleingTasksDemo.java
@@ -0,0 +1,19 @@
+package concurency.scheduling_tasks;
+
+import java.util.concurrent.*;
+
+/**
+ * Created by grigort on 7/17/2019.
+ */
+public class ScheduleingTasksDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Runnable command = () -> System.out.println("Hi");
+        Callable<String> callable = () -> "JAVA";
+        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+
+        ScheduledFuture< String> future = scheduledExecutorService.schedule(callable,2,TimeUnit.MINUTES);
+        System.out.println(future.get());
+       // scheduledExecutorService.scheduleAtFixedRate(command,0,30, TimeUnit.SECONDS);
+        scheduledExecutorService.scheduleWithFixedDelay(command,0,20,TimeUnit.SECONDS);
+    }
+}
